2727d45d4dcc8244529b59e8ee510045efe14b68
liuhaijun <714779582@qq.com>
2021-08-08 15:20:12 +0800

    修改Demo01

--- git_demo04/src/cn/xh/git/Demo01.java
+++ git_demo04/src/cn/xh/git/Demo01.java
@@ -1,7 +1,10 @@
 package cn.xh.git;
 
+
 public class Demo01 {
-    public static void main(String args[]){
+    public static void main(String[] args){
+
         System.out.println("h");
+        System.out.println("hh");
     }
 }
